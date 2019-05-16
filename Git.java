d22d7268e8aa25903d505cffc5cae3d7a6860440
AllamS20 <49028526+AllamS20@users.noreply.github.com>
2019-05-16 13:44:39 -0400

    Add files via upload

---
+++ Git.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Git {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.print("Type two integers");
+		Scanner input = new Scanner(System.in);
+		int one = input.nextInt();
+		int two = input.nextInt();
+		int avg = one + two;
+		System.out.print(avg);
+		
+	}
+
+}
